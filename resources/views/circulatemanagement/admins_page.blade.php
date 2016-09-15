b22f11f6d883701473955d07351ed7b8f54481e5
tomori777999 <tomori79@outlook.jp>
2016-09-15 14:48:58 +0900

    'sideverでgetﾊﾟﾗﾒｰﾀによるコンテンツの変更処理を追記'

--- resources/views/circulatemanagement/admins_page.blade.php
+++ resources/views/circulatemanagement/admins_page.blade.php
@@ -1,17 +1,32 @@
+<?php $contents = $_GET['ctt'] ?>
 @extends('layouts.app')
 
 @section('content')
-<div class="col-md-3 col-lg-2 sidebar-offcanvas" id="sidebar" role="navigation">
-    <ul class="nav nav-pills nav-stacked">
-        <li class="nav-item"><a class="nav-link" href="#">貸出log一覧</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">PC情報編集</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Menu3</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Menu4</a></li>
-    </ul>
-</div>
-[コンテンツ]</br>
-ここには、log画面時にはドロップダウンリスト[users][computers][rownum]を、pc編集画面時にはボタン[add]を右寄せで設置...したい
+<div class="row row-offcanvas row-offcanvas-left">
+    <div class="col-md-3 col-lg-2 sidebar-offcanvas" id="sidebar" role="navigation">
+        <ul class="nav nav-pills nav-stacked">
+            <li class="nav-item"><a class="nav-link" href="admins?ctt=logs">貸出log一覧</a></li>
+            <li class="nav-item"><a class="nav-link" href="admins?ctt=computerss">PC情報編集</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Menu3</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Menu4</a></li>
+        </ul>
+    </div>
 
-@include('layouts.admins_contents.logs')
+    <div class="col-md-9 col-lg-10 main">
+
+      [コンテンツ]</br>
+      ここには、log画面時にはドロップダウンリスト[users][computers][rownum]を、pc編集画面時にはボタン[add]を右寄せで設置...したい
+      @if($contents == "logs")
+        @include('layouts.admins_contents.logs')
+      @else
+        @include('layouts.admins_contents.computers')
+      @endif
+
+
+
+
+    </div>
+        <!--/main col-->
+</div>
 
 @endsection
