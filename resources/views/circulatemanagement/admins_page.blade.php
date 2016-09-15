62ad769840d418c7f105d5d9d03e3f4058d63a42
tomori777999 <tomori79@outlook.jp>
2016-09-15 13:21:11 +0900

    'logコンテンツのinclude化'

--- resources/views/circulatemanagement/admins_page.blade.php
+++ resources/views/circulatemanagement/admins_page.blade.php
@@ -11,56 +11,7 @@
 </div>
 [コンテンツ]</br>
 ここには、log画面時にはドロップダウンリスト[users][computers][rownum]を、pc編集画面時にはボタン[add]を右寄せで設置...したい
-<div class="col-lg-9 col-md-8">
-                    <div class="table-responsive">
-                        <table class="table table-striped">
-                            <thead class="thead-inverse">
-                                <tr>
-                                    <th>Column1</th>
-                                    <th>Column2</th>
-                                    <th>Column3</th>
-                                    <th>Column4</th>
-                                    <th>Column5</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Row1_1</td>
-                                    <td>Row1_2</td>
-                                    <td>Row1_3</td>
-                                    <td>Row1_4</td>
-                                    <td>Row1_5</td>
-                                </tr>
-                                <tr>
-                                    <td>Row2_1</td>
-                                    <td>Row2_2</td>
-                                    <td>Row2_3</td>
-                                    <td>Row2_4</td>
-                                    <td>Row2_5</td>
-                                </tr>
-                                <tr>
-                                    <td>Row3_1</td>
-                                    <td>Row3_2</td>
-                                    <td>Row3_3</td>
-                                    <td>Row3_4</td>
-                                    <td>Row3_5</td>
-                                </tr>
-                                <tr>
-                                    <td>Row4_1</td>
-                                    <td>Row4_2</td>
-                                    <td>Row4_3</td>
-                                    <td>Row4_4</td>
-                                    <td>Row4_5</td>
-                                </tr>
-                                <tr>
-                                    <td>Row5_1</td>
-                                    <td>Row5_2</td>
-                                    <td>Row5_3</td>
-                                    <td>Row5_4</td>
-                                    <td>Row5_5</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+
+@include('layouts.admins_contents.logs')
+
 @endsection
