4b0303c51f43cea84750d2d20ddf8e35756976a9
tomori777999 <tomori79@outlook.jp>
2016-09-16 14:28:57 +0900

    'controller周りの修正'

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,9 +13,18 @@ class AdminController extends Controller
 
   public function index()
   {
-
-    
     return view('admin.index');
   }
+  public function show(){
+     $logs = $this->Log
+                  ->leftJoin('users','user_id','=','id')
+                  ->leftJoin('computers','computer_id','=','id')
+                  ->orderBy('id','desc')
+                  ->take(20)
+                  ->value('name','computer_names','circulation_flag','create_at');
+    return view('admin.index')->with(compact('logs',$logs));
+  }
+
+
 
 }
