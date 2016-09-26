cf92981cf235a38b7f4f52369d0bb96f3db17dd6
tomori777999 <tomori79@outlook.jp>
2016-09-26 19:24:27 +0900

    'adminlogin後userloginに遷移してしまう'

--- app/Http/Controllers/Auth/AdminAuthController.php
+++ app/Http/Controllers/Auth/AdminAuthController.php
@@ -40,6 +40,7 @@ class AdminAuthController extends Controller
     public function __construct()
     {
         $this->middleware('guest:admin', ['except' => 'logout']);
+        // $this->middleware('auth:admin');
     }
 
     /**
